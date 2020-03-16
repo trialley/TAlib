f2fac7d633be6afbeb6291ff61042f0dcab5fa1c
root <root@DESKTOP-CIAI9HC.localdomain>
2020-03-17 00:20:09 +0800

    reconsitution

---
+++ src/Reactor/Poller.cpp
@@ -0,0 +1,28 @@
+#include <assert.h>
+
+#include <Reactor/Poller.hpp>
+#include <Reactor/Channel.hpp>
+#include <Reactor/EventLoop.hpp>
+
+using namespace muduo;
+
+Poller::Poller(EventLoop* loop)
+  : p_Loop(loop)
+{
+}
+
+Poller::~Poller()
+{
+}
+
+bool Poller::hasChannel(Channel* channel) const
+{
+  assertInLoopThread();
+  ChannelMap::const_iterator it = m_channels.find(channel->fd());
+  return it != m_channels.end() && it->second == channel;
+}
+
+void Poller::assertInLoopThread() const
+{
+  p_Loop->assertInLoopThread();
+}
